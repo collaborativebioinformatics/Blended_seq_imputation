5a371200801a459bbd3f39668ad44e060eaf17d8
Phil Greer <pgreer@mac.com>
2025-03-04 15:26:48 -0500

    fixed dockerfile

--- ref_scripts/st2.2.sh
+++ ref_scripts/st2.2.sh
@@ -2,9 +2,13 @@
 
 for CHR in {20,21,22,}; do
 
+# Normaile making all multialleelic site biallelic
+ bcftools norm -m -any ~/ariel_research/lp-wgs_research/hgdp_ref/phased_haplotypes_v2/hgdp1kgp_chr${CHR}.filtered.SNV_INDEL.phased.shapeit5.bcf \
+     -Ob --threads 4 > hgdp1kgp_chr${CHR}.filtered.SNV_INDEL.phased.shapeit5.norm.bcf 
+ bcftools index -f hgdp1kgp_chr${CHR}.filtered.SNV_INDEL.phased.shapeit5.norm.bcf
 
- bcftools view -G -Oz -o reference_panel/HGDP_ref.chr${CHR}.sites.vcf.gz \
-     ~/ariel_research/lp-wgs_research/hgdp_ref/phased_haplotypes_v2/hgdp1kgp_chr${CHR}.filtered.SNV_INDEL.phased.shapeit5.bcf
+# extract site information
+ bcftools view -G -Oz -o reference_panel/HGDP_ref.chr${CHR}.sites.vcf.gz hgdp1kgp_chr${CHR}.filtered.SNV_INDEL.phased.shapeit5.norm.bcf
  bcftools index -f reference_panel/HGDP_ref.chr${CHR}.sites.vcf.gz
 
 done
